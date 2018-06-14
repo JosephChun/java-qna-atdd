efafb69c3b1fe1f7975647ab0ed2d1fedd72f05b
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-06-14 15:17:11 +0900

    /question/{id}에 대한 test 추가

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,13 +1,16 @@
 package codesquad.web;
 
 
+import codesquad.domain.Question;
 import codesquad.domain.User;
 import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +19,7 @@ import javax.annotation.Resource;
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
+
     private static final Logger log = LoggerFactory.getLogger(QuestionController.class);
 
     @Resource(name = "qnaService")
@@ -25,4 +29,23 @@ public class QuestionController {
     public String form() {
         return "/qna/form";
     }
+
+    @PostMapping("")
+    public String createQna(@LoginUser User loginUser, Question question) {
+        qnaService.create(loginUser, question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("question", qnaService.findById(id).get());
+        return "/qna/show";
+    }
+
+    @GetMapping("/{id}/form")
+    public String update(@PathVariable Long id, Model model) {
+        model.addAttribute("question", qnaService.findById(id).get());
+        return "/qna/updateForm";
+    }
+
 }
--- src/test/java/codesquad/web/QuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -1,30 +1,40 @@
 package codesquad.web;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import codesquad.domain.QuestionRepository;
 import codesquad.domain.UserRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
 public class QuestionAcceptanceTest extends AcceptanceTest {
-
-    @Autowired
-    QuestionRepository questionRepository;
-
-    @Autowired
-    UserRepository userRepository;
 
     @Test
     public void form() {
         ResponseEntity<String> response = template().getForEntity("/questions/form", String.class);
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    public void home() {
+        ResponseEntity<String> response = template().getForEntity("/", String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    public void show() {
+        ResponseEntity<String> response = template().getForEntity("/questions/1", String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+
+        assertThat(response.getBody().contains("dino title"), is(true));
+        assertThat(response.getBody().contains("dino contents"), is(true));
     }
 
 }
